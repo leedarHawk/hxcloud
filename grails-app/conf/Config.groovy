21beccbaa063d088baa76df22b2d80051f114ce4
zhangyuetao <zhangyuetao2011@gmail.com>
2016-02-12 15:11:58 +0800

    check in db

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -115,3 +115,16 @@ log4j.main = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+grails {
+    mail {
+        host = "smtp.qq.com"
+        port = 465
+        username = "275691475@qq.com"
+        password = "820218"
+        props = ["mail.smtp.auth":"true",
+                 "mail.smtp.socketFactory.port":"465",
+                 "mail.smtp.socketFactory.class":"javax.net.ssl.SSLSocketFactory",
+                 "mail.smtp.socketFactory.fallback":"false"]
+    }
+}
